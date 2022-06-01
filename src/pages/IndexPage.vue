2ad5aadcfed87b3ae6ba1b8fba94a03fb3d40aab
SJ <tohinata@naver.com>
2022-06-01 17:01:06 +0900

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-right">
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
