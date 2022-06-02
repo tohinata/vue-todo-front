614e30815de2e967c80e5808d159b379191ca592
SJ <tohinata@naver.com>
2022-06-02 23:13:57 +0900

    MyTask 컴포넌트 추가

---
+++ src/components/MyTask.vue
@@ -0,0 +1,15 @@
+<template>
+  <li>
+    <div>{{ task3.id }} {{ task3.name }} - {{ index }}</div>
+    <small>{{ task3.dueDate }} @ {{ task3.dueTime }}</small>
+    <button @click="deleteTask(index)">X</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["task3", "index"],
+};
+</script>
+
+<style scoped></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,13 @@
 <template>
   <q-page padding>
     <ul>
-      <li v-for="(task, index) in tasks" :key="(task, index)">
-        <div>{{ task.name }} - {{ index }}</div>
-        <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
-        <button @click="deleteTask(index)">X</button>
-      </li>
+      <task2
+        v-for="(task, index) in tasks"
+        :key="task.id"
+        :task3="task"
+        :index="index"
+      >
+      </task2>
     </ul>
   </q-page>
 </template>
@@ -18,16 +20,19 @@ export default defineComponent({
     return {
       tasks: [
         {
+          id: 1,
           name: "Go to shop",
           dueDate: "2022/05/01",
           dueTime: "18:30",
         },
         {
+          id: 2,
           name: "Get bananas",
           dueDate: "2022/05/05",
           dueTime: "12:00",
         },
         {
+          id: 3,
           name: "Get apples",
           dueDate: "2022/05/15",
           dueTime: "18:25",
@@ -39,6 +44,9 @@ export default defineComponent({
     deleteTask(index) {
       this.tasks.splice(index, 1);
     },
+  },
+  components: {
+    task2: require("src/components/MyTask.vue").default,
   },
 });
 </script>
